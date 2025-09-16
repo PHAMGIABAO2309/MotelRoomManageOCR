63337c5657d5de210cb5206437c719ef0b92416f
Daniel <hieutiti1992103@gmail.com>
2025-09-16 20:20:18 +0700

    a

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend!" });
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
